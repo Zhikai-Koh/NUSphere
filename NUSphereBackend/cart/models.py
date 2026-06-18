47af1aa8efa0c07c992c59c90208654c39751fb4
unknown <zhikai2004@gmail.com>
2026-06-18 16:12:54 +0800

    Added backend logic to Carts

--- NUSphereBackend/cart/models.py
+++ NUSphereBackend/cart/models.py
@@ -1,9 +1,21 @@
 from django.db import models
+from django.contrib.auth.models import User
+from listings.models import Listing  # Pulls your existing product details
+
+class Cart(models.Model):
+    #OneToOneField make each user have exactly one cart row
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='cart')
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.user.username}'s Cart"
 
 class CartItem(models.Model):
+    # Links each row to a parent Cart table row
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE, related_name='items')
+    # Links each row to a specific product
+    product = models.ForeignKey(Listing, on_delete=models.CASCADE)
     quantity = models.PositiveIntegerField(default=1)
-    item_name = models.CharField(max_length=255, default="")
-    item_price = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
-    
+
     def __str__(self):
-        return f"Item Number {self.id}: Product {self.item_name} (x{self.quantity})"
+        return f"{self.quantity} x {self.product.title} in {self.cart.user.username}'s cart"
--- NUSphereBackend/cart/serializers.py
+++ NUSphereBackend/cart/serializers.py
@@ -1,7 +1,21 @@
 from rest_framework import serializers
-from .models import CartItem
+from .models import Cart, CartItem
+from listings.serializers import ListingSerializer
 
 class CartItemSerializer(serializers.ModelSerializer):
+    #This embeds the full product details (title, price, image) inside the response.
+    #read-only true means dat this field is only when a GET request is sent, a POST request will ignore the field
+    product_details = ListingSerializer(source='product', read_only=True)
+
     class Meta:
         model = CartItem
-        fields = ['id', 'item_name', 'quantity', 'item_price']
+        fields = ['id', 'product', 'product_details', 'quantity']
+
+class CartSerializer(serializers.ModelSerializer):
+    #Tells Django to lookup all CartItem with column "cart" that matches the current Cart row's ID.
+    #It knows to look at the "cart" column because in models.py, the foreignkey has related_name = "items" which match the variable name.
+    items = CartItemSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Cart
+        fields = ['id', 'user', 'items']
