961e38e49946406a723145cdf74b5f9479342e21
Koh Zhi Kai <zhikai2004@gmail.com>
2026-07-04 09:38:37 +0800

    Fixing pending listing image getting

--- NUSphereFrontEnd/src/OpenMarket/PendingListing.jsx
+++ NUSphereFrontEnd/src/OpenMarket/PendingListing.jsx
@@ -65,7 +65,7 @@ export function PendingListings(){
                     className="listing-card"
                     >
                         {pending.listingItem__listing__image && (
-                            <img src={listing.image.startsWith('http') ? listing.image : `${API_BASE_URL}/media/${listing.image}`} 
+                            <img src={pending.listingItem__listing__image.startsWith('http') ? pending.listingItem__listing__image : `${API_BASE_URL}${pending.listingItem__listing__image}`} 
                             alt={pending.listingItem__listing__item_name}
                             className="listing-image"
                             />
