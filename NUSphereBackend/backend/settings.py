12f717475530249caa668ef06114b9f8485297a6
Koh Zhi Kai <zhikai2004@gmail.com>
2026-07-19 14:45:54 +0800

    Prep for deployment

--- NUSphereBackend/backend/settings.py
+++ NUSphereBackend/backend/settings.py
@@ -23,11 +23,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-ydas29=6oyub0n-%sob&o^e)6!p8531$)!cn%=gg=*sr9esn(c'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 # Application definition
 
